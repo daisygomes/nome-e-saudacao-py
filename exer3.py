28e1a5c0b57f8032bd7975d4d6b7d3bf84bc92e8
daisygomes <daisygomesca@icloud.com>
2025-11-13 08:50:41 -0300

    Add files via upload

---
+++ exer3.py
@@ -0,0 +1,5 @@
+print("------Inicia a soma de dois números-------")
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um outro número: "))
+soma = n1 + n2
+print(f"A soma entre {n1} e {n2} é igual a {soma}")
